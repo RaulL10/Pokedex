862ad9b355f6cfb9274741ebb8f13dab32c24c61
RaulL10 <lopezraul1410@gmail.com>
2022-04-09 01:29:24 -0700

    added pokemon class

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+class Pokemon:
+    def __init__(self, name, type, ability, description):
+        self.name = name
+        self.type = type
+        self.ability = ability
+        self.description = description
+
+pokemons = [
+    Pokemon('Bulbasaur', 'Grass', 'Razor Leaf', 'There is a plant seed on its back right from the day its born. The see slowly grows larger.'),
+    Pokemon('Charmander', 'Fire', 'Ember', 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.'),
+    Pokemon('Squirtle', 'Water', 'Water Gun', 'When it retracts its long neck into its shell, it squirts out water with vigorous force.'),
+    Pokemon('Pikachu', 'Electric', 'Thunderbolt' 'Pikachu that can generate powerful electricity and have cheek sacs that are extra soft and super stretchy.'),
+]
+
+
+
 def home(request):
     return render(request, 'home.html')
 
