8a45235f0aa43fd7e168e80a8454a088e0cb85fa
RaulL10 <lopezraul1410@gmail.com>
2022-04-11 20:19:46 -0700

    added a create template

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/',  views.about, name='about'),
-    path('pokemon/', views.pokemon_index, name='index'),
+    path('pokemons/', views.pokemons_index, name='index'),
+    path('pokemons/<int:pokemon_id>/', views.pokemons_detail, name='detail'),
+    path('pokemons/create/', views.PokemonCreate.as_view(), name='pokemons_create'),
+#     path('pokemons/<int:pk>/update', views.PokemonUpdate.as_view(), name='pokemons_update'),
+#     path('pokemons/<int:pk>/delete', views.PokemonDelete.as_view(), name='pokemons_delete')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,19 +3,19 @@ from .models import Pokemon
 # Create your views here.
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-class Pokemon:
-    def __init__(self, name, type, ability, description):
-        self.name = name
-        self.type = type
-        self.ability = ability
-        self.description = description
+# class Pokemon:
+#     def __init__(self, name, type, ability, description):
+#         self.name = name
+#         self.type = type
+#         self.ability = ability
+#         self.description = description
 
-pokemons = [
-    Pokemon('Bulbasaur', 'Grass', 'Razor Leaf', 'There is a plant seed on its back right from the day its born. The see slowly grows larger.'),
-    Pokemon('Charmander', 'Fire', 'Ember', 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.'),
-    Pokemon('Squirtle', 'Water', 'Water Gun', 'When it retracts its long neck into its shell, it squirts out water with vigorous force.'),
-    Pokemon('Pikachu', 'Electric', 'Thunderbolt', 'Pikachu that can generate powerful electricity and have cheek sacs that are extra soft and super stretchy.'),
-]
+# pokemons = [
+#     Pokemon('Bulbasaur', 'Grass', 'Razor Leaf', 'There is a plant seed on its back right from the day its born. The see slowly grows larger.'),
+#     Pokemon('Charmander', 'Fire', 'Ember', 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.'),
+#     Pokemon('Squirtle', 'Water', 'Water Gun', 'When it retracts its long neck into its shell, it squirts out water with vigorous force.'),
+#     Pokemon('Pikachu', 'Electric', 'Thunderbolt', 'Pikachu that can generate powerful electricity and have cheek sacs that are extra soft and super stretchy.'),
+# ]
 
 
 
@@ -25,6 +25,14 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-def pokemon_index(request):
+def pokemons_index(request):
     pokemons = Pokemon.objects.all()
     return render(request, 'pokemon/index.html', {'pokemons': pokemons})
+
+def pokemons_detail(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id=pokemon_id)
+    return render(request, 'pokemon/detail.html', { 'pokemon': pokemon})
+
+class PokemonCreate(CreateView):
+    model = Pokemon
+    fields = '__all__'
