e5c17aaef81c058026cadb268faadceb1de9d743
RaulL10 <lopezraul1410@gmail.com>
2022-04-12 17:15:13 -0700

    craeted training model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.forms import IntegerField
 from django.urls import reverse
 # Create your models here.
+
+
+HOURS = (
+    ('M', 'Morning'),
+    ('A', 'Afternoon'),
+    ('E', 'Evening'),
+)
+
 
 class Pokemon(models.Model):
     name = models.CharField(max_length=100)
@@ -13,3 +22,18 @@ class Pokemon(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'pokemon_id': self.id})
+
+
+class Training(models.Model):
+    time = models.CharField(
+        max_length=3,
+        choices=HOURS,
+        default=HOURS[0][0]
+    )
+    type = models.CharField(max_length=200)
+    date = models.DateField()
+
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_time_display()} on {self.date}"
