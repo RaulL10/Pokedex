8f7c130108dbe3053c9cae87f3b05b8fb2114262
RaulL10 <lopezraul1410@gmail.com>
2022-04-13 23:18:52 -0700

    registered pokeballs to adin site

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Pokemon, Training
+from .models import Pokemon, Training, Pokeball
 # Register your models here.
 
 admin.site.register(Pokemon)
 admin.site.register(Training)
+admin.site.register(Pokeball)
