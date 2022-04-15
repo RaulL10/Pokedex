a69d3d293557707fca934a7ccb8b2492f50d9b09
RaulL10 <lopezraul1410@gmail.com>
2022-04-15 11:34:45 -0700

    added user model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.forms import IntegerField
 from django.urls import reverse
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -28,6 +29,7 @@ class Pokemon(models.Model):
     ability = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
     pokeballs = models.ManyToManyField(Pokeball)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -57,3 +59,4 @@ class Photo(models.Model):
 
     def __str__(self):
         return f"Photo for pokemon_id: {self.pokemon_id} @{self.url}"
+        
